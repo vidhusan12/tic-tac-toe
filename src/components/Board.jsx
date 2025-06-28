a4a894b2e516e0d0487b2346f137ecf0671af959
vidhusan12 <vidhusan709@gmail.com>
2025-06-28 14:28:12 +1200

    added next player and winner status

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -7,13 +7,15 @@ const Board = () => {
 
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(squares);
 
   function handleSquareClick(index) {
+    if(winner || squares[index]) return;
     const newSquares = squares.slice()
     if (newSquares[index]) return; // if the sqaure is already filled do nothing
-    if(xIsNext === true){
-       newSquares[index] = "X";
-    } else{
+    if (xIsNext === true) {
+      newSquares[index] = "X";
+    } else {
       newSquares[index] = "O"
     }
     setXIsNext(!xIsNext);
@@ -21,18 +23,49 @@ const Board = () => {
 
   }
 
-  return (
-    <div className='board'>
-      {
-        squares.map((value, index) =>
-          <Square
-            key={index}
-            value={value}
-            onClick={() => handleSquareClick(index)}
-          />
-        )
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let line of lines) {
+      const [a, b, c] = line;
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]
       }
-    </div>
+    }
+    return null;
+  }
+
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <>
+      <div className='status'>{status}</div>
+      <div className='board'>
+        {
+          squares.map((value, index) =>
+            <Square
+              key={index}
+              value={value}
+              onClick={() => handleSquareClick(index)}
+            />
+          )
+        }
+      </div>
+    </>
   )
 }
 
